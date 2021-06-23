941af3e5fb942d0089c5983136edbda647ccbd93
Alessandro Beninati <alessandrobeninati98@gmail.com>
2021-06-23 08:26:30 +0200

    minor

--- basis/grid.py
+++ basis/grid.py
@@ -23,7 +23,7 @@ class SqGrid:
         self.area= xv[0,-1]*yv[-1,0]
 
    
-    def evaluate(self,k):
+    def _evaluate(self,k):
         """"Returns a grid representing the codomain of the piecewise-continuos basis
         linear function corresponding to the k-th nodal point.
         """
@@ -31,8 +31,8 @@ class SqGrid:
         return res
 
     
-    def check(self,k):
-        """Continuos piecewise linear basis functions shoudl map their corresponding nodes to
+    def _check(self,k):
+        """Continuos piecewise linear basis functions should map their corresponding nodes to
         the value: 1. The computational grid doesn't necesessarily generate the nodal point.
         This function has been created only to check approximately whether the function has been coded correctly.
         """ 
@@ -44,9 +44,11 @@ class SqGrid:
         else:
             raise EstimationError("Not correct.")
 
-    def machine_errors(self,k):
-        """Returns coordinate of points where the function maps to a negative value.
-           This has been used often to check whether the function has been defined correctly.
+    def _machine_errors(self,k):
+        """Returns coordinate of points where the function 
+           finite_basis maps to a negative value.
+           This has been used often to 
+           check whether the function has been defined correctly.
            """
         res=self.evaluate(k)
         logic=[]
@@ -56,45 +58,44 @@ class SqGrid:
                     logic.append(f"({i},{j})")
         return np.transpose(logic)
 
-    def clean_evaluate(self,k):
-        res=self.evaluate(k)
-        for i in range(len(res)):
-            for j in range(len(res[0])):
-                if res[i,j]<0:
-                    res[i,j]=0
-        return res
+    # def _clean_evaluate(self,k):
+    #     res=self.evaluate(k)
+    #     for i in range(len(res)):
+    #         for j in range(len(res[0])):
+    #             if res[i,j]<0:
+    #                 res[i,j]=0
+    #     return res
 
     """Integrators"""
-    def MC(self,k):
-        """Integrate the continuos piecewise linear function corresponding to the 
-           k-th node over the whole domain using a method similar to Monte-Carlo.
-           It's probably the method that requires the most computational power and on the
-           other side it probably doesn't give good results.
-           """
-        return np.sum(self.evaluate(k))/np.size(self.evaluate(k))*self.area
+    # def _MC(self,k):
+    #     """Integrate the continuos piecewise linear function corresponding to the 
+    #        k-th node over the whole domain using a method similar to Monte-Carlo.
+    #        It's probably the method that requires the most computational power and on the
+    #        other side it probably doesn't give good results.
+    #        """
+    #     return np.sum(self.evaluate(k))/np.size(self.evaluate(k))*self.area
 
-    def quad(self,k):
-        """Integrate using a quadrature rule over the x-axis and then brings back to a 
-           Monte Carlo 1-D integration. Thsi requires less computation and it's probably 
-           more precise than the previous one.
-           """
-        x_i=self.nodes[k][0]
-        y_j=self.nodes[k][1]
-        I=[]
-        for y in self.mesh[1][:,0]:
-            I.append(quad(phi_ij,self.mesh[0][0,0],self.mesh[0][0,-1],args=(y,x_i,y_j,self.h))[0])
-        return np.sum(I)/np.size(I)*self.mesh[1][-1,0]
+    # def _integr(self,k):
+    #     """Integrate using a quadrature rule over the x-axis and then brings back to a 
+    #        Monte Carlo 1-D integration. Thsi requires less computation and it's probably 
+    #        more precise than the previous one.
+    #        """
+    #     x_i=self.nodes[k][0]
+    #     y_j=self.nodes[k][1]
+    #     I=[]
+    #     for y in self.mesh[1][:,0]:
+    #         I.append(quad(phi_ij,self.mesh[0][0,0],self.mesh[0][0,-1],args=(y,x_i,y_j,self.h))[0])
+    #     return np.sum(I)/np.size(I)*self.mesh[1][-1,0]
     
-    def quadrature(self,k,n,f):
+    def _inner(self,k,f=lambda x,y: 1,n=7):
         """Gauss-Legendre approximation (n degree) of the integral of
            the k-th piecewise continuos linear function times 
            an arbitrary function f over the whole domain of the problem.
-
            In short: 
+           let ( u , v )= int_{Omega} u v
            The method approximates:
-
-                    ( phi_ij(x,y) , f(x,y) ) 
-                    
+                    ( phi_k(x,y) , f(x,y) ) if 2 or 3 inputs are given
+                    ||phi_k(x,y)||^2 if 1 input is given
                     """
         a,b=self.mesh[0][0,0],self.mesh[0][0,-1]
         c,d=self.mesh[1][0,0],self.mesh[1][-1,0]
@@ -104,6 +105,7 @@ class SqGrid:
     
 class EstimationError(Exception):
     pass
+
 
     
 
