f571dcd86487bdb1e47f0c2bc09f43f8b2988853
Alessandro Beninati <alessandrobeninati98@gmail.com>
2021-06-26 09:55:03 +0200

    n

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-    name="ASS",
+    name="1051811_Assessment",
     version="0.1",
     packages=find_packages(),
-    install_requires=[ "numpy"]
+    install_requires=[ "numpy", "scipy","numbers"]
 )
